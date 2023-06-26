c117658d08c96f13a0900d825957be96d0596e05
deboraferrarids2 <deboraferrarids2@gmail.com>
2023-06-25 22:37:19 -0300

    added command populate products into docker-compose

--- burgerstore/settings.py
+++ burgerstore/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
